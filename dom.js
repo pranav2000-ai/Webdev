1289402e72c242b36898d10160858a39ba4292eb
KingKong <ppkul2000@gmail.com>
2021-07-21 20:58:36 +0530

    adding all files

---
+++ dom.js
@@ -0,0 +1,5 @@
+var headerTitle = document.getElementById('header-title');
+headerTitle.innerText = "HELLO"
+headerTitle.style.borderBottom = "solid 3px #000";
+items = document.getElementsByTagName('h2');
+items[0].style.fontWeight = "900";
